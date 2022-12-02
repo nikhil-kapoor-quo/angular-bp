e4a4a362aa383bb5b1add07cd4d313ddfa80700e
Nikhil Kapoor <46014825+nikhil-kapoor-quo@users.noreply.github.com>
2022-12-02 10:21:28 +0530

    Added fixes

--- src/app/auth/forgot-password/forgot-password.component.ts
+++ src/app/auth/forgot-password/forgot-password.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { UntypedFormGroup, UntypedFormControl, Validators } from '@angular/forms';
+import { Validators, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-forgot-password',
@@ -8,14 +8,14 @@ import { UntypedFormGroup, UntypedFormControl, Validators } from '@angular/forms
   styleUrls: ['./forgot-password.component.scss']
 })
 export class ForgotPasswordComponent implements OnInit {
-  public forgotForm: UntypedFormGroup;
+  public forgotForm: FormGroup;
   public showAlert: boolean;
 
   constructor(private router: Router) {}
 
   ngOnInit() {
-    this.forgotForm = new UntypedFormGroup({
-      email: new UntypedFormControl('', Validators.required)
+    this.forgotForm = new FormGroup({
+      email: new FormControl('', Validators.required)
     });
   }
 
