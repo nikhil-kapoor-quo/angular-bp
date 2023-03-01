d7a62077a85ae346032623a84a184e85f23d0a6a
Nikhil Kapoor <46014825+nikhil-kapoor-quo@users.noreply.github.com>
2023-03-01 15:57:27 +0530

    Added pre commit hook

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { AuthRoutingModule } from './auth-routing.module';
-import { ChangePasswordComponent } from './change-password/change-password.component';
 
 @NgModule({
-  declarations: [
-  ],
+  declarations: [],
   imports: [AuthRoutingModule]
 })
 export class AuthModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,29 +2,19 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AlertModule } from 'ngx-bootstrap/alert';
-import { TooltipModule } from 'ngx-bootstrap/tooltip';
-
-import { DefaultModalComponent } from './components/default-modal/default-modal.component';
-
 @NgModule({
-  declarations: [DefaultModalComponent],
+  declarations: [],
   imports: [
     CommonModule,
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
-    AlertModule.forRoot(),
-    TooltipModule.forRoot()
   ],
   exports: [
     CommonModule,
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
-    AlertModule,
-    TooltipModule,
-    DefaultModalComponent
   ]
 })
 export class SharedModule { }
